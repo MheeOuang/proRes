13b7e47e76a488b702315fb52714fb553272596d
MheeOuang <poowave1734@gmail.com>
2023-03-16 12:49:15 +0700

    new order
    ทำหน้าorderใหม่

--- src/app/page/order/order.component.ts
+++ src/app/page/order/order.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/service/data.service';
 import { Convert as orderCvt, Order} from 'src/app/model/order.model';
+import { Convert as FoodOrderCvt, FoodOrder} from 'src/app/model/foodOrder';
 
 @Component({
   selector: 'app-order',
@@ -10,10 +11,20 @@ import { Convert as orderCvt, Order} from 'src/app/model/order.model';
 })
 export class OrderComponent {
   orders = Array<Order>();
+  foodOrders = Array<FoodOrder>();
   constructor(private dataService: DataService,private http: HttpClient){
     http.get(dataService.apiEnpoint + '/order').subscribe((data: any)=>{
       this.orders = orderCvt.toOrder(JSON.stringify(data));
       console.log(this.orders);
+    });
+
+
+  }
+
+  show(oid : number){
+    this.http.get(this.dataService.apiEnpoint+'/order/info/' + oid).subscribe((data:any)=>{
+      this.foodOrders = FoodOrderCvt.toFoodOrder(JSON.stringify(data));
+      console.log(this.foodOrders);
     });
   }
 }
